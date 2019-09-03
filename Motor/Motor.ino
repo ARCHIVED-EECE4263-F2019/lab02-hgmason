7f36273f97c2b7016a1b1a97afd7143322c1113d
Mason <hgmason@Lipscomb.edu>
2019-09-03 12:46:42 -0500

    added good comments

--- Motor/Motor.ino
+++ Motor/Motor.ino
@@ -1,29 +1,36 @@
+//#define DEBUG
+
+// Constants for speed, direction, and button pins
 #define FORWARD D5
 #define BACKWARD D6
-#define S_BUTTON D1
-#define D_BUTTON D0
+#define SPEED_BUTTON D1
+#define DIRECTION_BUTTON D0
 #define FAST (1024 - 1)
 #define SLOW FAST/2
 #define OFF 0
-#define debounce_delay 50
-#define change_speed digitalRead(S_BUTTON)
-#define change_direction digitalRead(D_BUTTON)
+
+// Using functions instead of variables so that
+// the value is updated each time its called
+#define change_speed digitalRead(SPEED_BUTTON)
+#define change_direction digitalRead(DIRECTION_BUTTON)
+
+// Making lists of all possible speeds and directions
+// and indexes to keep track of the current ones
+// (Simpler than a bunch of if/else or switch statements)
 int speeds[3] = {OFF, SLOW, FAST};
 int directions[2] = {FORWARD, BACKWARD};
 int speed_i = 0;
 int direction_i = 0;
 
-/* need to work on the debouncing thing
- * and need to fix the all off thing
- */
-
 void setup() {
-  // initialize pins for output
+  // initialize pins for output and input
   pinMode(FORWARD, OUTPUT);
   pinMode(BACKWARD, OUTPUT);
-  pinMode(S_BUTTON, INPUT);
-  pinMode(D_BUTTON, INPUT);
+  pinMode(SPEED_BUTTON, INPUT);
+  pinMode(DIRECTION_BUTTON, INPUT);
+  #ifdef DEBUG
   Serial.begin(9600);
+  #endif
 }
 
 // the loop function runs over and over again forever
@@ -32,28 +39,31 @@ void loop() {
     delay(15);
     //Serial.println("Button pressed.");
     if (change_speed && !change_direction){
+      #ifdef DEBUG
       Serial.println("CHANGE SPEED");
+      #endif
       speed_i = (speed_i + 1)%3;
     }
     if (change_direction && !change_speed){
+      #ifdef DEBUG
       Serial.println("CHANGE DIRECTION");
+      #endif
       direction_i = (direction_i + 1)%2;
     }
     while (change_speed || change_direction){
       if (change_direction && change_speed){
+        #ifdef DEBUG
         Serial.println("STOP ALL");
+        #endif
         speed_i = 0;
       }
-      go(directions[direction_i],speeds[speed_i]);
+      // inside the while loop so that updates are near immediate
+      go(directions[direction_i],speeds[speed_i]); 
     }
-    //Serial.println("Button released.");
-    //Serial.print("DIRECTION: ");
-    //Serial.println(directions[direction_i]);
-    //Serial.print("SPEED: ");
-    //Serial.println(speeds[speed_i]);
   }
 }
 
+//the general GO function
 void go(int direction, int speed){
   if (direction == FORWARD){
     go_forward(speed);
@@ -67,6 +77,8 @@ void go(int direction, int speed){
   }
 }
 
+// handle the specific directions to ensure
+// both direction pins aren't high at the same time
 void go_forward(int speed){
   analogWrite(BACKWARD, OFF);
   analogWrite(FORWARD, speed);
